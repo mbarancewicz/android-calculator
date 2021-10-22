1d3f34e3802753c1095963f098aa9640e9c49a05
mbarancewicz <44418145+mbarancewicz@users.noreply.github.com>
2021-10-22 21:44:22 +0200

    insert number and validate number

--- app/src/main/java/com/example/calculator/StandardCalc.kt
+++ app/src/main/java/com/example/calculator/StandardCalc.kt
@@ -1,11 +1,48 @@
 package com.example.calculator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class StandardCalc : AppCompatActivity() {
+    private var numbers = mutableListOf<Char>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_standard)
+    }
+
+    fun insertNumber(v: View) {
+        val char = v.tag.toString().single()
+        if(isValid(char)) {
+            numbers.add(char)
+            updateDisplay(v)
+        }
+    }
+
+    fun evaluate(view: View) {}
+
+    fun clear(v: View) {
+        numbers = mutableListOf()
+        updateDisplay(v)
+    }
+
+    fun clearAll(v: View) {
+        clear(v)
+    }
+
+    private fun updateDisplay(v: View) {
+        val display = v.rootView.findViewById<TextView>(R.id.display)
+        display.text = numbers.joinToString("")
+    }
+
+    private fun isValid(c: Char): Boolean {
+        val validPoint = c != '.' || !numbers.contains('.')
+        val notStartWithZero = if(numbers.isEmpty()) c != '.' else true
+        val nonZeroAtBeginning = numbers.isNotEmpty() || c != '0'
+        val notTooLong = numbers.size < 15
+
+        return validPoint && notStartWithZero && nonZeroAtBeginning && notTooLong
     }
 }
